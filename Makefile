c12229d7a5320b32fc042678a401793513e709bd
dnguye12 <duchuyng051@gmail.com>
2022-04-28 14:37:19 +0200

    kinda done

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ all: main
 
 
 main: animal.o coord.o ensemble.o grille.o population.o jeu.o main.o
-	g++ -std=c++11 -Wall -o main animal.o coord.o ensemble.o grille.o population.o jeu.o main.o
+	g++ -std=c++11 -Wall -o main animal.o coord.o ensemble.o grille.o population.o jeu.o menu.o main.o
 
 # fichiers objets
 main.o: main.cpp
-	g++ -std=c++11 -Wall -c main.cpp
+	info-111 g++ main.cpp -o main -lsfml-system -lsfml-window -lsfml-graphics
 animal.o: animal.cpp animal.hpp
 	g++ -std=c++11 -Wall -c animal.cpp
 coord.o: coord.cpp coord.hpp
@@ -19,6 +19,8 @@ population.o: population.cpp population.hpp
 	g++ -std=c++11 -Wall -c population.cpp
 jeu.o: jeu.cpp jeu.hpp
 	g++ -std=c++11 -Wall -c jeu.cpp
+menu.o: menu.cpp menu.hpp
+	info-111 g++ menu.cpp -o menu -lsfml-system -lsfml-window -lsfml-graphics
 
 test: test.o coord.o
 	g++ -o test test.o coord.o
--- menu.cpp
+++ menu.cpp
@@ -107,28 +107,36 @@ void Menu::MainMenu()
     text1.setFont(font);
     text1.setString("| Space |: Pause/Start\n| N |: Create a new board\n| Esc |: Quit the game");
     text1.setCharacterSize(12);
-    text1.setPosition(20, TAILLEGRILLE * cell_size);
+    text1.setPosition(20, TAILLEGRILLE * cell_size + 25);
 
     text2.setFont(font);
     text2.setString(str);
     text2.setCharacterSize(12);
-    text2.setPosition(TAILLEGRILLE * cell_size - 200, TAILLEGRILLE * cell_size);
+    text2.setPosition(TAILLEGRILLE * cell_size - 200, TAILLEGRILLE * cell_size + 25);
 
     text3.setFont(font);
     text3.setString(str1);
     text3.setCharacterSize(12);
-    text3.setPosition(TAILLEGRILLE * cell_size - 200, TAILLEGRILLE * cell_size + 12);
+    text3.setPosition(TAILLEGRILLE * cell_size - 200, TAILLEGRILLE * cell_size + 37);
 
     text4.setFont(font);
     text4.setString(str2);
     text4.setCharacterSize(12);
-    text4.setPosition(TAILLEGRILLE * cell_size - 200, TAILLEGRILLE * cell_size + 24);
+    text4.setPosition(TAILLEGRILLE * cell_size - 200, TAILLEGRILLE * cell_size + 49);
+
+    RectangleShape cell_legend_lapin(Vector2f(cell_size,cell_size));
+    const Color rabbit_color(0,255,255);
+    cell_legend_lapin.setFillColor(rabbit_color);
+    cell_legend_lapin.setPosition(TAILLEGRILLE * cell_size - 224, TAILLEGRILLE * cell_size + 40);
+
+    RectangleShape cell_legend_renard(Vector2f(cell_size,cell_size));
+    const Color renard_color(255,0,0);
+    cell_legend_renard.setFillColor(renard_color);
+    cell_legend_renard.setPosition(TAILLEGRILLE * cell_size - 224, TAILLEGRILLE * cell_size + 52);
 
 
 
-
-
-    RenderWindow window(VideoMode(TAILLEGRILLE * cell_size,TAILLEGRILLE * cell_size + 220), "Main Menu");
+    RenderWindow window(VideoMode(TAILLEGRILLE * cell_size,TAILLEGRILLE * cell_size + 125), "Main Menu");
 
     Menu menu(window.getSize().x, window.getSize().y);
 
@@ -161,11 +169,11 @@ void Menu::MainMenu()
                         srand(time(0));
                         Clock clk;
                         clk.restart();
-                        const float update_delay = 0.1;
+                        const float update_delay = 0.01;
 
 
 
-                        sf::RenderWindow window1(sf::VideoMode(TAILLEGRILLE * cell_size, TAILLEGRILLE * cell_size + 220), "Simulation Display");
+                        sf::RenderWindow window1(sf::VideoMode(TAILLEGRILLE * cell_size, TAILLEGRILLE * cell_size + 100), "Simulation Display");
 
                         int level[TAILLEGRILLE * TAILLEGRILLE];
                         for(int l = 0; l < TAILLEGRILLE; l++)
@@ -265,7 +273,7 @@ void Menu::MainMenu()
                                 }
                                 text2.setString(str);
                                 str1 = "Lapin Population = " + to_string(j.lapinPop());
-                                    str2 = "Renard Population = " + to_string(j.renardPop());
+                                str2 = "Renard Population = " + to_string(j.renardPop());
                                 text3.setString(str1);
                                 text4.setString(str2);
                                 map.load(tile_name, sf::Vector2u(cell_size, cell_size), level, TAILLEGRILLE, TAILLEGRILLE);
@@ -278,6 +286,8 @@ void Menu::MainMenu()
                             window1.draw(text2);
                             window1.draw(text3);
                             window1.draw(text4);
+                            window1.draw(cell_legend_lapin);
+                            window1.draw(cell_legend_renard);
                             window1.display();
 
 
